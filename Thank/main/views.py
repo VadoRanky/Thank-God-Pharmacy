83d2bf941be58f08c3ded7695f2d861280fe89f7
ADEBOYE KAYODE <adeboyekayode112@gmail.com>
2026-06-10 06:09:43 -0700

    Add new HTML templates for post, services, and single post pages
    - Created post.html for displaying financial insights and articles.
    - Developed services.html to outline the financial services offered by VaultEdge.
    - Introduced single-post.html for detailed views of individual articles, including investment strategies.

--- Thank/main/views.py
+++ Thank/main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html"),
+
+def contact(request):
+    return render(request, "con.html"),
+
+def service(request):
+    return render(request, "serve.html"),
+
+def element(request):
+    return render(request, "ele.html"),
+
+def post(request):
+    return render(request, "post.html"),
